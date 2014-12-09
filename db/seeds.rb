fabf2e7664989c97e716fe8d8939080eb07844b9
Alex Lang <langalex4@gmail.com>
2014-12-09 15:32:05 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Team.destroy_all
+Game.destroy_all
+Pick.destroy_all
+
+team_1 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_2 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_3 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_4 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_5 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_6 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_7 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_8 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_9 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_10 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_11 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+team_12 = Team.create(school: Faker::Company.name, mascot: Faker::Lorem.words(2).join(' '), location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", wins: rand(5..10), losses: rand(0..5))
+
+
+
+Game.create(title: "#{Faker::Commerce.department(2)} Bowl", location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", datetime: Faker::Date.forward , away: team_1, home: team_2, championship: false)
+Game.create(title: "#{Faker::Commerce.department(2)} Bowl", location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", datetime: Faker::Date.forward , away: team_3, home: team_4, championship: false)
+Game.create(title: "#{Faker::Commerce.department(2)} Bowl", location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", datetime: Faker::Date.forward , away: team_5, home: team_6, championship: false)
+Game.create(title: "#{Faker::Commerce.department(2)} Bowl", location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", datetime: Faker::Date.forward , away: team_7, home: team_8, championship: false)
+Game.create(title: "#{Faker::Commerce.department(2)} Bowl", location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", datetime: Faker::Date.forward , away: team_9, home: team_10, championship: false)
+Game.create(title: "#{Faker::Commerce.department(2)} Bowl", location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", datetime: Faker::Date.forward , away: team_11, home: team_12, championship: false)
